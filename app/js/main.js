d3a36be5ea91dd6affec44e38ea9c39a43363508
Jonathan D. Baker <jonathan@voltaic.io>
2015-09-06 13:01:36 -0600

    Prototype 'photos' app.

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,23 @@
 angular.module("voltaic", [
+    "restangular",
     "ui.router",
     "voltaic.cfg",
-    "voltaic.index"
+    "voltaic.index",
+    "voltaic.photos"
 ])
 
 
 .config([
         "$httpProvider",
         "$urlRouterProvider",
-        function($httpProvider, $urlRouterProvider) {
+        "RestangularProvider",
+        function($httpProvider, $urlRouterProvider, RestangularProvider) {
 
-    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common.Accept = "application/json";
     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 
     $urlRouterProvider.when("", "/");
     $urlRouterProvider.otherwise("/404");
+
+    RestangularProvider.setBaseUrl("http://127.0.0.1:8000/v1");
 }]);
---
+++ app/js/photos/app.js
@@ -0,0 +1,38 @@
+angular.module("voltaic.photos", [])
+
+
+.config(["$stateProvider", function($stateProvider) {
+    $stateProvider
+        .state("photo_detail", {
+            controller: "PhotoDetailCtrl",
+            templateUrl: "js/photos/views/detail.html",
+            title: "Photo",
+            url: "/photos/:id"
+        })
+        .state("photo_list", {
+            controller: "PhotoListCtrl",
+            templateUrl: "js/photos/views/list.html",
+            title: "Photos",
+            url: "/photos"
+        });
+}])
+
+
+.controller("PhotoDetailCtrl", [
+        "$scope", "$stateParams", "Photo", function($scope, $stateParams, Photo) {
+    Photo.one($stateParams.id).get().then(function(photo) {
+        $scope.photo = photo;
+    });
+}])
+
+
+.controller("PhotoListCtrl", ["$scope", "Photo", function($scope, Photo) {
+    Photo.getList().then(function(photos) {
+        $scope.photos = photos;
+    });
+}])
+
+
+.factory("Photo", ["Restangular", function(Restangular) {
+    return Restangular.service("photos");
+}]);
